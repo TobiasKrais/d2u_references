39c176ebbcef3ccade47f0cea0efeb1cfd4e0746
tobias <tobias@UbuntuXPS>
2019-06-05 21:46:32 +0200

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -27,6 +27,10 @@ $d2u_module_manager->showManagerList();
 <h2>Support</h2>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_references" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.0.7-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.0.6:</p>
 <ul>
 	<li>Bugfix: Fatal error beim Speichern verursacht durch die URL Addon Version 2 Anpassungen behoben.</li>
