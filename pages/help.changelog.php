79e20390f9813c509651f9c638ba4d937a88eb96
Tobias Krais <tk@design-to-use.de>
2026-05-26 17:27:24 +0200

    finanlize version

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <h2>Changelog</h2>
-<p>1.2.1-DEV:</p>
+<p>1.2.1:</p>
 <ul>
 	<li>Backend: Abbrechen-Buttons in Referenz- und Tagformularen fuehren jetzt wieder zur Liste.</li>
 	<li>Backend: CSRF-Schutz fuer Speichern-, Loesch- und Statusaktionen der Referenzverwaltung ergaenzt.</li>
